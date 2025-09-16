e3515d39a0a96f9b0dbd29b55e1532f33ff29864
Radhesh Pai <radhesh.ratnakar.s87@kalvium.community>
2025-09-16 10:18:42 +0530

    mongoDB code addition

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = 3000 || process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error(`Error connecting to MongoDB: ${err}`);
+});
 
 app.use(express.static('static'));
 
